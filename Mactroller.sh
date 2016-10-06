e9df012c4fb8de0bd4dbd46b123d8e0cebc71966
Adrian Buford <adrianbuford@gmail.com>
2016-10-06 14:02:51 -0500

    Create Mactroller.sh

---
+++ Mactroller.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+mailformADMIN=/tmp/1form.txt
+myemail=YOUREMAILADDRESS
+count=$(cat /tmp/counter.txt | grep -c ^)
+adminmac=cc:79:4a:41:6d:04
+
+arp-scan --localnet > /tmp/arpscan.txt
+
+if cat /tmp/arpscan.txt | grep -q $adminmac; then
+
+echo Admin interface located on network. No action required.
+
+exit 0
+
+else
+
+echo Count Added >> /tmp/counter.txt
+
+./shutdown_counter.sh
+
+fi 
+
+exit 0
